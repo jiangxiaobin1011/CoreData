90602f1df5ea9c9ed1501e8c0c2230d09087b5c7
lorne <280077576@qq.com>
2019-03-14 10:14:35 +0800

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by lorne on 2019/3/14.
+//  Copyright © 2019 ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
